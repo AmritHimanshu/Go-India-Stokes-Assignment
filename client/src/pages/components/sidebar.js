d63fefe5f7ac3f933acae5ea513114eb36fa1188
AmritHimanshu <amrit.ewp@gmail.com>
2024-03-20 15:00:08 +0530

    fixed sidebar

--- client/src/pages/components/sidebar.js
+++ client/src/pages/components/sidebar.js
@@ -11,8 +11,8 @@ function Sidebar() {
     const [isTrue, setIsTrue] = useState(false);
 
     return (
-        <>
-            <div className={`bg-[#0369a1] flex justify-between ${!isTrue && 'xl:w-[40%] lg:w-[60%]'} ${isTrue && 'xl:w-[0px] lg:w-[0px]'} duration-300 h-[100vh] absolute lg:relative`}>
+        <div className={`h-[100vh] flex absolute lg:relative ${!isTrue && 'xl:w-[40%] lg:w-[60%] md:w-[40%]'} ${isTrue && 'xl:w-[0px] lg:w-[0px] md:w-[0px]'} duration-300`}>
+            <div className={`bg-[#0369a1] flex justify-between w-full`}>
                 <div className={`text-white w-full ${isTrue && 'hidden'} block`}>
                     <div className='p-5 flex items-center justify-between space-x-10 lg:space-x-0'>
                         <div className='space-x-5 lg:space-x-5 flex items-center'>
@@ -38,11 +38,12 @@ function Sidebar() {
                     <div className='p-3 pl-14 text-[14px] xl:text-xl text-md font-bold cursor-pointer hover:bg-white hover:text-[#0369a1] duration-300'>News/Interview</div>
                 </div>
 
-            <div className={`h-[100vh] flex items-center bg-white ${isTrue && 'bg-[#0369a1 text-white'}`}>{!isTrue && <ArrowBackIosNewIcon style={{ cursor: 'pointer' }} onClick={() => setIsTrue(true)} />}</div>
             </div>
 
-            <div className={`h-[100vh] flex items-center ${isTrue && 'bg-[#0369a1] text-white'}`}>{isTrue && <ArrowForwardIosIcon style={{ cursor: 'pointer' }} onClick={() => setIsTrue(false)} />}</div>
-        </>
+            {!isTrue && <div className='h-[100vh] flex items-center bg-transparent'><div className='py-10 bg-[#0369a1] text-white cursor-pointer rounded-lg' onClick={() => setIsTrue(true)}><ArrowBackIosNewIcon /></div></div>}
+
+            {isTrue && <div className='h-[100vh] flex items-center bg-transparen'><div div className='py-10 bg-[#0369a1] text-white cursor-pointer rounded-lg' onClick={() => setIsTrue(false)}><ArrowForwardIosIcon /></div></div>}
+        </div>
     )
 }
 
